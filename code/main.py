0028b7fd40f011dbd9daebe4117e128bfd8e1248
ahkoru <gabgonzaga28@gmail.com>
2025-05-16 17:53:21 +0800

    changed how notes are placed

--- code/main.py
+++ code/main.py
@@ -1,10 +1,5 @@
 from sprites import *
-from functions import read_osu_map
-from tkinter.filedialog import askopenfilename
-from os import makedirs
-from os.path import join, dirname, isfile
-import json
-import shutil
+from classes import *
 
 
 class Game():
@@ -18,27 +13,24 @@ class Game():
         #? Setup
         self.clock = pygame.Clock()
         
-        self.is_running = True
         self.is_playing = False
+        
+        #? Sprite/Sprite groups
+        self.main_menu_sprites = pygame.sprite.Group()
+        self.note_sprites = pygame.sprite.Group()
         
         
         #? Loading stuff
         self.kirby_surf = pygame.image.load("assets/Kirb.png").convert_alpha()
         self.font = pygame.Font(None, 50)
-        
-        
-        #? Sprite/Sprite groups
-        self.all_sprites = pygame.sprite.Group()
-       
+        self.button = Button(self.main_menu_sprites, "assets/Kirb.png", True, (WINDOW_WIDTH/2, WINDOW_LENGTH/2))
        
        #? Notes
-        self.notes: dict[str, list[Note]] = {}
+        self.notes: dict[str, list[Note]] = {"lane 1":[], "lane 2":[], "lane 3":[], "lane 4":[]}
         
-        
-        #? Maps
-        self.import_map()
-        self.load_map()
-        
+    
+    def text(self, content):
+        pass
     
     def import_map(self):
         beatmap_path = askopenfilename()                        #* Absolute path
@@ -68,22 +60,22 @@ class Game():
     def load_map(self):
         #? Open the saved beatmap
         with open(self.map_path, 'r', encoding='utf-8') as file:
-            beatmap = json.load(file)
+            self.beatmap: dict[str, list[int]] = json.load(file)
         
         #? Create Note objects based on the beatmap data
-        for lane, y_values in enumerate(beatmap.values(), 1):
-            lane_num = f"lane {lane}"
-            self.notes[lane_num] = []
-            for y in y_values:
-                note = Note(self.all_sprites, self.kirby_surf, ((WINDOW_WIDTH / 5) * lane, -y + 500), 1000, self.notes, lane_num)
-                self.notes[lane_num].append(note)
+        # for lane, y_values in enumerate(self.beatmap.values(), 1):
+        #     lane_num = f"lane {lane}"
+        #     self.notes[lane_num] = []
+        #     for y in y_values:
+        #         note = Note(self.note_sprites, self.kirby_surf, (WINDOW_WIDTH / 5) * lane, 1000, self.notes, lane_num)
+        #         self.notes[lane_num].append(note)
         
         #? Load the music
         self.music = pygame.mixer.Sound(join(dirname(self.map_path), 'audio.mp3'))
         self.music.set_volume(0.5)
         self.load_time = pygame.time.get_ticks()
+        if self.load_time > 1000: return
 
-    
     def pressed(self, event: pygame.event.Event):
         #? key name(a,s,j,k)
         key_name = pygame.key.name(event.key)
@@ -115,36 +107,67 @@ class Game():
             self.display.blit(text_surf, text_rect)
 
     def run(self):
-        while self.is_running:
+        while True:
             #? Game clock
             dt = self.clock.tick() / 1000
             
             #? Event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.is_running = False
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.button.is_clicked() and pygame.mouse.get_pressed()[0]:
+                        self.play()
+
+            #? Updates
+            self.main_menu_sprites.update(dt)
+                
+            #? Draw
+            self.display.fill('black')
+            self.main_menu_sprites.draw(self.display)
+       
+            pygame.display.flip()
+            
+    
+    def play(self):
+        running = True
+        self.import_map()
+        self.load_map()
+        self.load_time = pygame.time.get_ticks()
+        self.music.play()
+        while running:
+            dt = self.clock.tick() / 1000
+        
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
                 elif event.type == pygame.KEYDOWN:
                     self.pressed(event)
+                    
+                    if event.key == pygame.K_ESCAPE:
+                        running = False
             
-            if pygame.time.get_ticks() - self.load_time > 500:
-                #? Updates
-                self.all_sprites.update(dt)
-                
-                #? Draw
-                self.display.fill('black')
-                pygame.draw.line(self.display, 'white', (0, LINE_HEIGHT), (WINDOW_WIDTH, LINE_HEIGHT), 10)
-                self.all_sprites.draw(self.display)
-                self.highlight()
-
-                if not self.is_playing:
-                    self.music.play()
-                    self.is_playing = True
-                
+            #? Update
+            for lane, y_values in enumerate(self.beatmap.values(), 1):
+                lane_num = f"lane {lane}"
+                if y_values[-1] <= pygame.time.get_ticks() - self.load_time:
+                    note = Note(self.note_sprites, self.kirby_surf, (WINDOW_WIDTH / 5) * lane, 1000, self.notes, lane_num)
+                    self.notes[lane_num].append(note)
+                    self.beatmap[lane_num].pop()
+            self.note_sprites.update(dt)
+            
+            #? Draw
+            self.display.fill('blue')
+            pygame.draw.line(self.display, 'white', (0, LINE_HEIGHT), (WINDOW_WIDTH, LINE_HEIGHT), 10)
+            self.note_sprites.draw(self.display)
             
             pygame.display.flip()
             
-        pygame.quit()
-    
+        self.note_sprites.empty()
+        self.music.stop()
+            
 
 if __name__ == '__main__':
     game = Game()
